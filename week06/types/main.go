97cb40c4575b94e863f88c09503673cd80e415fb
hong <seojun7789@gmail.com>
2025-09-29 11:53:51 +0900

    v0.2 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,10 +6,18 @@ import (
 )
 
 func main() {
-	var name string
-	//name = "Kim Inha"
-	//var name="Kim Inha"
-	//name := 2.71
-	name = "Kim inha"
+	// var name string
+	// name = "Kim Inha"
+
+	// var name = "Kim Inha"
+
+	// name := "Kim Inha"
+	// name := 2.71
+
+	// name = "Kim Inha"
+	// name = 2.71
+
+	var name float64
+	name = 2.71
 	fmt.Println(name, reflect.TypeOf(name))
 }
