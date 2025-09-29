9850c44b07938c596ba60d0bc1315a23fb3aebd8
hong <seojun7789@gmail.com>
2025-09-29 12:13:39 +0900

    v0.4 tpyes/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	var f64 float64
-	var str string
-	var i32 int32
-	var b bool
-	fmt.Println(f64, reflect.TypeOf(f64))
-	fmt.Println(str, reflect.TypeOf(str))
-	fmt.Println(i32, reflect.TypeOf(i32))
-	fmt.Println(b, reflect.TypeOf(b))
+	//var f64 float64
+	//fmt.Println(f64, reflect.TypeOf(f64))
+	totalPrice := 500
+	fmt.Println(totalPrice)
+	//totalPrice := 1000
+	//fmt.Println(totalPrice)
 }
