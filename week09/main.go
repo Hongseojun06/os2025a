8ae2fc637f813c37bf608adc879bd4f5505f1e69
hong <seojun7789@gmail.com>
2025-10-27 11:54:50 +0900

    function/main.go v0.31 pointer

--- week09/main.go
+++ week09/main.go
@@ -1,32 +1,37 @@
-// Package keyboard reads user input from the keyboard.
-package keyboard
+package main
 
 import (
 	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-// GetFloat reads a floating-point number from the keyboard.
-// It returns the number read and any error encountered.
 func GetFloat() (float64, error) {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
 		return 0, err
 	}
-
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
 }
+
 func main() {
-	fmt.Print("점수 입력:")
+	fmt.Print("점수 입력 : ")
 	score, err := GetFloat()
 	if err != nil {
 		log.Fatal(err)
 	}
 	status := ""
 	if score >= 60 {
-		status = "합격"
+		status = "합격!"
 	} else {
 		status = "불합격"
 	}
