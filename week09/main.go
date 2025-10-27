6024688374aa4fee28ce3fe9d5043e3def8c35c8
hong <seojun7789@gmail.com>
2025-10-27 11:27:42 +0900

    function/main.go v0.2 pointer

--- week09/main.go
+++ week09/main.go
@@ -4,16 +4,17 @@ import (
 	"fmt"
 )
 
-func swap(first int, second int) {
+func swap(first *int, second *int) {
 	temp := 0
-	temp = first
-	first = second
-	second = temp
+	temp = *first
+	*first = *second
+	*second = temp
+	fmt.Println(first, second)
 }
 
 func main() {
 	var a, b int = 10, 20
 	fmt.Println(a, b)
-	swap(a, b)
+	swap(&a, &b) //call by pointer
 	fmt.Println(a, b)
 }
