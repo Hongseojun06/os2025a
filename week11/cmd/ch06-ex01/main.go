97df84c0da507fa14c209ba1a388eaa8ea0f061d
hong <seojun7789@gmail.com>
2025-11-10 11:22:40 +0900

    week11 slice v0.2

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var subjects []string
-	subjects = make([]string, 3)
-	subjects[0] = "Go"
-	subjects[2] = "python"
+	subjects := []string{"Go", "", "python"} //initialized by slice litered
 
 	for _, subject := range subjects {
 		fmt.Println(subject)
